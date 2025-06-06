870023fa51eb8f13b17ec432916d70d2d39bf44d
NearCheck <nearchecklite@gmail.com>
2025-06-07 07:56:15 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyBRcN1Uwv9g5SYQzfdofc0T8UgYkOAipMY",
+    authDomain: "barkada-3c8c5.firebaseapp.com",
+    databaseURL: "https://barkada-3c8c5-default-rtdb.firebaseio.com",
+    projectId: "barkada-3c8c5",
+    storageBucket: "barkada-3c8c5.firebasestorage.app",
+    messagingSenderId: "484579003166",
+    appId: "1:484579003166:web:e1ab0da6c9f4d6f739570f",
+    measurementId: "G-NQ4JR7CC2T"
+};
